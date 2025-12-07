dde4b0ab081c0aaf0109a89daef700da18a9cfc8
Bùi Huy <huybq>
2025-12-07 20:13:39 +0700

    Update Image

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -5,8 +5,6 @@ import MusicPlayer from "@/components/MusicPlayer";
 
 const WEDDING_IMAGES = [
   "/images/wedding/1.JPG",
-  "/images/wedding/15.jpg",
-  "/images/wedding/16.JPG",
   "/images/wedding/2.JPG",
   "/images/wedding/3.JPG",
   "/images/wedding/4.jpg",
